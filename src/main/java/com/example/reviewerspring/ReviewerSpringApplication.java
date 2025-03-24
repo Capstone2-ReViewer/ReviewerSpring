c2687c493c9bd589cdd51b82d503f3bf817612f7
pangmo00 <rhkdah9090@gmail.com>
2025-03-24 16:23:46 +0900

    1

---
+++ src/main/java/com/example/reviewerspring/ReviewerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.reviewerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reviewerspring/ReviewerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reviewerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
